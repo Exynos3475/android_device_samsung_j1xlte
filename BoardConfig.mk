07383ed8390b126ca8511ce96b10a88e6fd7fdeb
TBM <mateo.trueba@gmail.com>
2019-10-24 17:27:47 -0300

    Enable dex-preoptimization
    This makes the boot sequence a lot more faster

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,9 @@ BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 # Release tools
 TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
 
+# Dex-preoptimization to speed up boot sequences
+WITH_DEXPREOPT := true
+
 # Recovery
 BOARD_HAS_DOWNLOAD_MODE := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.universal3475
