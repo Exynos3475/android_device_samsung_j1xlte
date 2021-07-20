de4856bfb9b1dd734ddade653b12fb1824ecd083
Seden Ecem <ttbpccorporation@outlook.com>
2021-07-20 13:46:57 +0300

    j1xlte: Downgrade audio hal to 2.0
    Signed-off-by: Seden Ecem <ttbpccorporation@outlook.com>

--- device.mk
+++ device.mk
@@ -29,8 +29,8 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     tinymix \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.service \
     audio.primary.default \
     audio.sec_primary.default \
